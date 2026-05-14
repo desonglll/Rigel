cec59273ec295919ad89c255cb679de86484f647
desonglll <lindesong666@163.com>
2026-05-14 15:44:42 +0800

    fix token authorization.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -39,7 +39,7 @@ class Api::V1::CommentsController < ActionController::API
   end
 
   def authenticate_user_by_token
-    token = request.headers["Authorization"]&.split(" ")&last
+    token = request.headers["Authorization"]&.split(" ")&.last
     @current_user = User.find_by(api_token: token)
     render json: { error: "Unauthorized" }, status: :unauthorized unless @current_user
   end
